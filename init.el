2923d6c851923ff2cca5c36b317364cdb7a1c11c
Adrian Teng <adrian@teng.io>
2014-12-17 23:45:06 +0000

    Add quick-reload init.el, open recent files

--- init.el
+++ init.el
@@ -4,12 +4,19 @@
 (package-initialize)
 
 
+;; Quick reload of init.el
+
+(global-set-key (kbd "<f12>") (lambda() (interactive)(load-file "~/.emacs.d/init.el")))
+
+
+;; UI related
+
 (add-to-list 'load-path "~/.emacs.d/elpa/darcula-theme-20141211.128")
 
 (require 'darcula-theme)
 
 (add-to-list 'default-frame-alist '(font .  "DejaVu Sans Mono-10.5" ))
-(set-frame-font   "DejaVu LGC Sans Mono-10.5" nil t)
+(set-frame-font   "DejaVu Sans Mono-10.5" nil t)
 
 
 ;; indentation
@@ -21,7 +28,33 @@
 (add-to-list 'load-path "~/.emacs.d/elpa/magit-20141214.1225")
 
 
+;; Autocomplete
+
 (add-hook 'prog-mode-hook 'electric-pair-mode)
 (add-hook 'prog-mode-hook 'company-mode)
+(global-set-key (kbd "M-SPC") 'company-complete)
 
-(global-set-key (kbd "C-SPC") 'company-complete)
+
+;; Recently opened files
+
+(require 'recentf)
+
+;; offer recently accessed files from the menu
+(recentf-mode t)
+
+;; remember this many files
+(setq recentf-max-saved-items 200)
+
+(defun ido-recentf-open ()
+  "Use `ido-completing-read' to \\[find-file] a recent file"
+  (interactive)
+  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+      (message "Opening file...")
+    (message "Aborting")))
+
+(global-set-key (kbd "C-x C-r") 'ido-recentf-open)
+
+
+;; git
+
+(global-set-key (kbd "C-c g") 'magit-status)
