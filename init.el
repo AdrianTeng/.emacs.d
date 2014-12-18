082a926dcc29bd8200e41fbf67d54696842dc89d
Adrian Teng <adrian@teng.io>
2014-12-18 22:50:39 +0000

    Add js2-mode

--- init.el
+++ init.el
@@ -136,5 +136,12 @@
 (set-face-foreground 'rainbow-delimiters-unmatched-face "red")
 
 
+;; javascript
+
+(add-hook 'js-mode-hook 'js2-minor-mode)
+(add-hook 'js2-mode-hook 'ac-js2-mode)
+
+
 (provide 'init)
 ;;; init.el ends here
+
