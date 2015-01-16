4fdb96839095c4a1906cb80b88833e134ca5e440
Adrian Teng <ateng@ahl.com>
2015-01-16 15:55:47 +0000

    Point flycheck to homedir jshintrc

--- init.el
+++ init.el
@@ -310,7 +310,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("c5898bd49bea497d5d356d698f82379c12c7325a3697604a9d1ad7f98c8e6647" default))))
+ '(custom-safe-themes (quote ("c5898bd49bea497d5d356d698f82379c12c7325a3697604a9d1ad7f98c8e6647" default)))
+ '(flycheck-jshintrc "~/.jshintrc"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
